2418b551a97d3935fdae36bb85a92284895df8ab
CharuDattPimple <charudattPimple@gmail.com>
2017-07-26 17:01:05 +0530

    Add files via upload

---
+++ test/specs/Title.spec.js
@@ -0,0 +1,10 @@
+// login.spec.js
+var expect = require('chai').expect;
+describe('Verify the Git hub page title', function () {
+    it('should display page title', function () {
+        browser.url('https://in.yahoo.com/');
+        var title = browser.getTitle()
+        console.log(title);
+        expect(title).to.contain('Yahoo');
+    });
+});
